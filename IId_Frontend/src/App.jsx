f2c71267ee67a967ceca81ff14f028030120424d
Ashish <ashishrohilla510@gmail.com>
2023-09-07 15:31:43 +0530

    adding backend and frontend

---
+++ IId_Frontend/src/App.jsx
@@ -0,0 +1,17 @@
+
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <div>
+      </div>
+      <p className="text-red-600 text-4xl">
+        Click on the Vite and React logos to learn more
+      </p>
+    </>
+  )
+}
+
+export default App
