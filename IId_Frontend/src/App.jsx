c8394d2ec956fcac6528ea805354df3468faa823
Rashi1703 <128238586+Rashi1703@users.noreply.github.com>
2023-09-09 20:36:59 +0530

    Update App.jsx

--- IId_Frontend/src/App.jsx
+++ IId_Frontend/src/App.jsx
@@ -1,17 +1,19 @@
-
-import './App.css'
-
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Biometric from "./components/Biometric/Biometric"
+import SearchingMatching from "./components/Biometric/SearchingMatching"
+import SecuritySetting from "./components/Setting/SecuritySetting"
 function App() {
-
   return (
-    <>
-      <div>
-      </div>
-      <p className="text-red-600 text-4xl">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <div className="App ">
+      <BrowserRouter>
+        <Routes>
+          <Route element={<Biometric/>} path="/Biometric" exact />
+          <Route element={<SearchingMatching/>} path="/SearchingMatching" exact />
+          <Route element={<SecuritySetting/>} path="/SecuritySetting" exact />
+           </Routes>
+      </BrowserRouter>
+    </div>
+  );
 }
 
-export default App
+export default App;
