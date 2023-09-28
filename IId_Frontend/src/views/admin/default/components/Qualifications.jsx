bd7fa800f0654f926aff184a9e7a4ec7642328f9
Nisha gera <nishaa.1803gera@gmail.com>
2023-09-28 18:04:01 +0530

    user profile

--- IId_Frontend/src/views/admin/default/components/Qualifications.jsx
+++ IId_Frontend/src/views/admin/default/components/Qualifications.jsx
@@ -1,95 +1,94 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
 
 const QualificationForm = () => {
   // Initialize state for qualifications
-  const [schoolQualification, setSchoolQualification] = useState('');
-  const [schoolGrade, setSchoolGrade] = useState('');
-  const [graduationQualification, setGraduationQualification] = useState('');
-  const [graduationGrade, setGraduationGrade] = useState('');
-  const [postGraduationQualification, setPostGraduationQualification] = useState('');
-  const [postGraduationGrade, setPostGraduationGrade] = useState('');
-
-  // Function to handle form submission
-  const handleSubmit = (e) => {
-    e.preventDefault();
-    // You can process the submitted data here, e.g., send it to a server.
-    // For this example, we'll just log the data.
-    console.log('School Qualification:', schoolQualification, 'Grade:', schoolGrade);
-    console.log('Graduation Qualification:', graduationQualification, 'Grade:', graduationGrade);
-    console.log('Post Graduation Qualification:', postGraduationQualification, 'Grade:', postGraduationGrade);
-  };
-
+  const [schoolQualification, setSchoolQualification] = useState("");
+  const [schoolGrade, setSchoolGrade] = useState("");
+  const [graduationQualification, setGraduationQualification] = useState("");
+  const [graduationGrade, setGraduationGrade] = useState("");
+  const [postGraduationQualification, setPostGraduationQualification] =
+    useState("");
+  const [postGraduationGrade, setPostGraduationGrade] = useState("");
+   
   return (
-    <div>
-      <h2>Qualification Form</h2>
-      <form onSubmit={handleSubmit}>
-        {/* School Qualification */}
-        <div>
-          <label htmlFor="schoolQualification">School Qualification:</label>
-          <input
-            type="text"
-            id="schoolQualification"
-            value={schoolQualification}
-            onChange={(e) => setSchoolQualification(e.target.value)}
-            required
-          />
-        </div>
-        <div>
-          <label htmlFor="schoolGrade">Grade:</label>
-          <input
-            type="text"
-            id="schoolGrade"
-            value={schoolGrade}
-            onChange={(e) => setSchoolGrade(e.target.value)}
-            required
-          />
-        </div>
+    <div className="">
+      <h2 className="text-lg font-bold">Qualification Form</h2>
 
-        {/* Graduation Qualification */}
-        <div>
-          <label htmlFor="graduationQualification">Graduation Qualification:</label>
-          <input
-            type="text"
-            id="graduationQualification"
-            value={graduationQualification}
-            onChange={(e) => setGraduationQualification(e.target.value)}
-            required
-          />
-        </div>
-        <div>
-          <label htmlFor="graduationGrade">Grade:</label>
-          <input
-            type="text"
-            id="graduationGrade"
-            value={graduationGrade}
-            onChange={(e) => setGraduationGrade(e.target.value)}
-            required
-          />
-        </div>
+      {/* School Qualification */}
+      <div className="m-2">
+        <label htmlFor="schoolQualification">School Qualification:</label>
+        <input
+          type="text"
+          id="schoolQualification"
+          value={schoolQualification}
+          onChange={(e) => setSchoolQualification(e.target.value)}
+          class="bg-darkBlue w-full ml-2 border border-2 border-white rounded-md p-2"
+          required
+        />
+      </div>
+      <div className="m-2">
+        <label htmlFor="schoolGrade">Grade:</label>
+        <input
+          type="text"
+          id="schoolGrade"
+          value={schoolGrade}
+          onChange={(e) => setSchoolGrade(e.target.value)}
+          class="bg-darkBlue w-full ml-2 border border-2 border-white rounded-md p-2"
+          required
+        />
+      </div>
 
-        {/* Post Graduation Qualification */}
-        <div>
-          <label htmlFor="postGraduationQualification">Post Graduation Qualification:</label>
-          <input
-            type="text"
-            id="postGraduationQualification"
-            value={postGraduationQualification}
-            onChange={(e) => setPostGraduationQualification(e.target.value)}
-            required
-          />
-        </div>
-        <div>
-          <label htmlFor="postGraduationGrade">Grade:</label>
-          <input
-            type="text"
-            id="postGraduationGrade"
-            value={postGraduationGrade}
-            onChange={(e) => setPostGraduationGrade(e.target.value)}
-            required
-          />
-        </div>
+      {/* Graduation Qualification */}
+      <div className="m-2">
+        <label htmlFor="graduationQualification">
+          Graduation Qualification:
+        </label>
+        <input
+          type="text"
+          id="graduationQualification"
+          value={graduationQualification}
+          onChange={(e) => setGraduationQualification(e.target.value)}
+          class="bg-darkBlue w-full ml-2 border border-2 border-white rounded-md p-2"
+          required
+        />
+      </div>
+      <div className="m-2">
+        <label htmlFor="graduationGrade">Grade:</label>
+        <input
+          type="text"
+          id="graduationGrade"
+          value={graduationGrade}
+          onChange={(e) => setGraduationGrade(e.target.value)}
+          class="bg-darkBlue w-full ml-2 border border-2 border-white rounded-md p-2"
+          required
+        />
+      </div>
 
-      </form>
+      {/* Post Graduation Qualification */}
+      <div className="m-2">
+        <label htmlFor="postGraduationQualification">
+          Post Graduation Qualification:
+        </label>
+        <input
+          type="text"
+          id="postGraduationQualification"
+          value={postGraduationQualification}
+          onChange={(e) => setPostGraduationQualification(e.target.value)}
+          class="bg-darkBlue w-full ml-2 border border-2 border-white rounded-md p-2"
+          required
+        />
+      </div>
+      <div className="m-2">
+        <label htmlFor="postGraduationGrade">Grade:</label>
+        <input
+          type="text"
+          id="postGraduationGrade"
+          value={postGraduationGrade}
+          onChange={(e) => setPostGraduationGrade(e.target.value)}
+          class="bg-darkBlue w-full ml-2 border border-2 border-white rounded-md p-2"
+          required
+        />
+      </div>
     </div>
   );
 };
--- IId_Frontend/src/views/admin/default/index.jsx
+++ IId_Frontend/src/views/admin/default/index.jsx
@@ -1,113 +1,112 @@
-import React, { useState } from 'react';
-import Skills from './components/skills';
-import Qualifications from './components/Qualifications';
-import MissionHistories from './components/MissionHistories';
+import React, { useState } from "react";
+import Skills from "./components/skills";
+import Qualifications from "./components/Qualifications";
+import MissionHistories from "./components/MissionHistories";
 
 const Dashboard = () => {
   // Define state variables for personal details
-  const [userName] = useState('Nisha Gera');
-  const [fullName, setFullName] = useState('');
-  const [gender, setGender] = useState('');
-  const [dateOfBirth, setDateOfBirth] = useState('');
-  const [phone, setPhone] = useState('');
-  const [email, setEmail] = useState('');
-  const [address, setAddress] = useState('');
-
-  // Handle form submission
-  const handleSubmit = (e) => {
-    e.preventDefault();
-    // You can add logic here to send the data to a backend server or update the state as needed.
-    // For now, let's log the data to the console.
-    
-    console.log('Full Name:', fullName);
-    console.log('Gender:', gender);
-    console.log('Date of Birth:', dateOfBirth);
-    console.log('Phone:', phone);
-    console.log('Email:', email);
-    console.log('Address:', address);
-  };
-
+  const [userName] = useState(" ");
+  const [fullName, setFullName] = useState("");
+  const [gender, setGender] = useState("");
+  const [dateOfBirth, setDateOfBirth] = useState("");
+  const [phone, setPhone] = useState("");
+  const [email, setEmail] = useState("");
+  const [address, setAddress] = useState("");
   return (
     <div>
-<form onSubmit={handleSubmit}></form>
-<div className="mt-5">
-<h1>Welcome, {userName}!</h1>
+      <div className="mt-5">
+        <h1 className="text-xl font-bolder">Welcome, {userName}!</h1>
+      </div>
+      <div class="grid grid-cols-2 gap-4">
+        {/* <!-- First row, first column --> */}
+        <div className="col-span-1 bg-darkBlue p-4 text-white rounded-[12px]">
+          <h2 className="text-lg font-bold">Personal Details</h2>
 
-</div>
-<div class="grid grid-cols-2 gap-4">
-{/* <!-- First row, first column --> */}
-<div class="col-span-1 bg-blue-200 p-4">
-<h2>Personal Details</h2>
-     
-        <label>
-          Full Name:
-          <input
-            type="text"
-            value={fullName}
-            onChange={(e) => setFullName(e.target.value)}
-          />
-        </label> <br />
-        <label>
-          Gender:
-          <input
-            type="text"
-            value={gender}
-            onChange={(e) => setGender(e.target.value)}
-          />
-        </label> <br />
-        <label>
-          Date of Birth:
-          <input
-            type="date"
-            value={dateOfBirth}
-            onChange={(e) => setDateOfBirth(e.target.value)}
-          />
-        </label> <br />
-        <label>
-          Contact Information:
-          <input
-            type="text"
-            placeholder="Phone"
-            value={phone}
-            onChange={(e) => setPhone(e.target.value)}
-          /> </label>
-           <br />
-           <label>
-        Email:
-          <input
-            type="email"
-            placeholder="Email"
-            value={email}
-            onChange={(e) => setEmail(e.target.value)}
-          />
-        </label> <br />
-        <label>
-          Address: <br />
-          <textarea
-            value={address}
-            onChange={(e) => setAddress(e.target.value)}
-          />
-        </label> <br />
-       
+          <div className="m-2">
+            <label className="">Full Name: </label>
+            <input
+              type="text"
+              value={fullName}
+              onChange={(e) => setFullName(e.target.value)}
+              class="bg-darkBlue w-full ml-2 border border-2 border-white rounded-md p-2"
+            />
+            <br />
+          </div>
+          <div className="m-2">
+            <label>Gender: </label>
+            <input
+              type="text"
+              value={gender}
+              onChange={(e) => setGender(e.target.value)}
+              class="bg-darkBlue w-full ml-2 border border-2 border-white rounded-md p-2"
+            />
+            <br />
+          </div>
+          <div className="m-2">
+            <label>Date of Birth: </label>
+            <input
+              type="date"
+              value={dateOfBirth}
+              className="bg-none"
+              onChange={(e) => setDateOfBirth(e.target.value)}
+              class="bg-darkBlue w-full ml-2 border border-2 border-white rounded-md p-2"
+            />{" "}
+            <br />
+          </div>
+          <div className="m-2">
+            <label>Contact Information:</label>
+            <input
+              type="text"
+              placeholder="Phone"
+              value={phone}
+              onChange={(e) => setPhone(e.target.value)}
+              class="bg-darkBlue w-full ml-2 border border-2 border-white rounded-md p-2"
+            />
+            <br />
+          </div>
+          <div className="m-2">
+            <label>Email: </label>
+            <input
+              type="email"
+              placeholder="Email"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+              class="bg-darkBlue w-full ml-2 border border-2 border-white rounded-md p-2"
+            />
+            <br />
+          </div>
+          <div className="m-2">
+            <label>Address: </label>
+            <textarea
+              value={address}
+              onChange={(e) => setAddress(e.target.value)}
+              class="bg-darkBlue w-full ml-2 border border-2 border-white rounded-md p-2"
+            />
+            <br />
+          </div>
+        </div>
 
-</div>
+        {/* <!-- First row, second column --> */}
+        <div class="col-span-1 bg-darkBlue p-4 text-white rounded-[12px]">
+          {" "}
+          <Qualifications />
+        </div>
 
-{/* <!-- First row, second column --> */}
-<div class="col-span-1 bg-green-200 p-4"><Skills/></div>
+        {/* <!-- Second row, first column --> */}
+        {/* <div class="col-span-1 row-span-2 bg-yellow-200 p-4">Column 1, Row 2</div> */}
 
-{/* <!-- Second row, first column --> */}
-{/* <div class="col-span-1 row-span-2 bg-yellow-200 p-4">Column 1, Row 2</div> */}
+        {/* <!-- second row, first column --> */}
+        <div class="col-span-1 bg-darkBlue p-4 text-white rounded-[12px]">
+          <Skills />
+        </div>
 
-{/* <!-- second row, first column --> */}
-<div class="col-span-1 bg-yellow-200 p-4"> <Qualifications/></div>
-
-{/* <!-- second row, second column --> */}
-<div class="col-span-1 bg-yellow-200 p-4"><MissionHistories/></div>
-
-</div>
-<form/>
-</div>
- 
+        {/* <!-- second row, second column --> */}
+        <div class="col-span-1 bg-darkBlue p-4 text-white rounded-[12px]">
+          <MissionHistories />
+        </div>
+      </div>
+      <form />
+    </div>
   );
 };
 
