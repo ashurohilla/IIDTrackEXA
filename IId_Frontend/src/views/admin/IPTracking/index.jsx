65ad1aa250220efdc476d35b38268c5bfd8fd26f
Nisha gera <nishaa.1803gera@gmail.com>
2023-10-12 22:08:13 +0530

    changes

--- IId_Frontend/src/views/admin/IPTracking/index.jsx
+++ IId_Frontend/src/views/admin/IPTracking/index.jsx
@@ -21,38 +21,41 @@ function IPTracking() {
   };
 
   return (
-    <div className="flex flex-col items-center justify-center min-h-screen border border-2">
+    <div className="flex flex-col items-center justify-center min-h-screen ">
       <br />
-      <h1 className="font-bold">
-        Enter the Device IP address Here to finf the Location
+      <div className=" items-center justify-center flex flex-col p-10 m-4" >
+      <h1 className="font-bold text-darkBlue text-xl">
+        Enter the Device IP address Here to find the Location.
       </h1>
-      <div>
+      <div className="flex flex-col items-center justify-center" >
+
       <input
         type="text"
         placeholder="Enter an IP address"
         value={ipAddress}
         onChange={handleInputChange}
-        className="text-lightBlue bg-gray"
-      />
-      <div>
+        className="text-gray-900 bg-gray text-center font-bold m-2"
+      /> 
+      <div className="m-2">
         <button
           onClick={fetchLocation}
-          className="bg-lightBlue text-white rounded-[10px] p-2 "
+          className="bg-lightBlue text-white rounded-[10px] p-2 text-center items-center font-bold"
         >
           Get Location
         </button>
       </div>
       </div>
       {location && (
-        <div>
-          <h2>Location Information:</h2>
-          <p>IP Address: {location.query}</p>
-          <p>Country: {location.country}</p>
-          <p>City: {location.city}</p>
-          <p>Latitude: {location.lat}</p>
-          <p>Longitude: {location.lon}</p>
+        <div className="m-1">
+          <h2 className="font-bold text-center text-darkBlue m-1" >Location Information:</h2>
+          <p className="text-darkBlue font-bold mb-1" >IP Address: <span className="underline font-light">{location.query}</span> </p>
+          <p className="text-darkBlue font-bold mb-1" >Country: <span className="underline font-light " > {location.country}</span> </p>
+          <p className="text-darkBlue font-bold mb-1" >City:  <span className="underline font-light " >{location.city}</span> </p>
+          <p className="text-darkBlue font-bold mb-1" >Latitude: <span className="underline font-light " >{location.lat}</span>  </p>
+          <p className="text-darkBlue font-bold mb-1" >Longitude <span className="underline font-light " > {location.lon}</span> </p>
         </div>
       )}
+      </div>
     </div>
   );
 }
--- IId_Frontend/src/views/admin/usermanagement/index.jsx
+++ IId_Frontend/src/views/admin/usermanagement/index.jsx
@@ -1,7 +1,7 @@
 import Dropdown from "./components/dropdown";
-import { useState,useEffect } from "react";
-import 'slick-carousel/slick/slick.css';
-import 'slick-carousel/slick/slick-theme.css';
+import { useState, useEffect } from "react";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
 
 // const UserManagement = () => {
 //   return (
@@ -35,109 +35,116 @@ import 'slick-carousel/slick/slick-theme.css';
 
 // export default UserManagement;
 
-
 const UserManagement = () => {
-    const [Profiledata, setProfiledata] = useState([]);
-    const [loading, setLoading] = useState(false);
-    const [error, setError] = useState(null);
-    const [currentPage, setCurrentPage] = useState(1);
-const itemsPerPage = 8; 
+  const [Profiledata, setProfiledata] = useState([]);
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState(null);
+  const [currentPage, setCurrentPage] = useState(1);
+  const itemsPerPage = 8;
 
-    useEffect(() => {
-        setLoading(true);
+  useEffect(() => {
+    setLoading(true);
 
-        const Profiledata = async (event) => {
-          try {
-            const response = await DashApi.Profiledata();
-            console.log(response);
-    
-            if (response.data && response.status === true) {
-              setError(response.data.msg);
-            } else {
-             
-                const sortedData = response.data.sort((a, b) => a.id - b.id);
-                setProfiledata(sortedData);
-                return setLoading(false);
-            }
+    const Profiledata = async (event) => {
+      try {
+        const response = await DashApi.Profiledata();
+        console.log(response);
 
-    
-            setLoading(false);
-          } catch (err) {
-            console.error(err);
-    
-            setLoading(false);
-    
-            if (err.response) {
-              setError(err.response.data.msg);
-            } else {
-              setError('There has been an error.');
-            }
-          }
-           
+        if (response.data && response.status === true) {
+          setError(response.data.msg);
+        } else {
+          const sortedData = response.data.sort((a, b) => a.id - b.id);
+          setProfiledata(sortedData);
+          return setLoading(false);
+        }
 
-            setLoading(false);
-        };
+        setLoading(false);
+      } catch (err) {
+        console.error(err);
 
-        Profiledata();
-    }, []);
+        setLoading(false);
 
-    // Calculate the range of items to display on the current page
-    const startIndex = (currentPage - 1) * itemsPerPage;
-    const endIndex = startIndex + itemsPerPage;
-    const itemsToDisplay = Profiledata.slice(startIndex, endIndex);
+        if (err.response) {
+          setError(err.response.data.msg);
+        } else {
+          setError("There has been an error.");
+        }
+      }
 
-    return (
-        <div className='page-container' >
+      setLoading(false);
+    };
 
-            <div className='content-container'>
-                <div className="pt-8 m-5 rounded-[10px] mt-20 overflow-auto rounded-lg shadow hidden md:block ">
-                    {/* Your Fetch Latest button */}
-                    {loading && <p>Loading...</p>}
-                    {error && <p>Error: {error}</p>}
-                    <div className="pt-5 relative overflow-x-auto">
-                        {itemsToDisplay.length > 0 ? (
-            <table className='justify-center w-full m-2 bg-darkBlue-500'>
-           <thead className="bg-gray-300 text-darkBlue-500">
-      <tr>
-        <th className='whitespace-no-wrap text-sm font-semibold tracking-wide text-left  p-4 '>SNo.</th>
-        <th className='whitespace-no-wrap text-sm font-semibold tracking-wide text-left  p-4'>Name</th>
-        <th className='whitespace-no-wrap text-sm font-semibold tracking-wide text-left  p-4'>EMail</th>
-        <th className='whitespace-no-wrap text-sm font-semibold tracking-wide text-left  p-4'>Contact</th>
-        <th className='whitespace-no-wrap text-sm font-semibold tracking-wide text-left  p-4'>Address</th>
-        </tr>
-    </thead>
-                               
-       <tbody className='divide-y divide-red-100 ' >
-         {itemsToDisplay.map((data, index) => (
-          <tr key={index} className='text-White-100' >
-          <td  className='whitespace-nowrap text-left p-4 text-sm '>{data.Sno}</td>  
-          <td  className='whitespace-nowrap text-left p-4 text-sm '>{data.name}</td>
-          <td  className='whitespace-nowrap text-left p-4 text-sm '>{data.email}</td>
-          <td  className='whitespace-nowrap text-left p-4 text-sm '>{data.contact}</td>
-          <td  className='whitespace-nowrap text-left p-4 text-sm '> 
-              {" "}
-              <div className="flex justify-center items-center h-screen">
-                <Dropdown />
-              </div>
-            </td>
-         
-           </tr>
-        ))}
-      </tbody>
-      </table>
-                       ) : (
-                            <p>No data found</p>
-                        )};
-                    </div>
+    Profiledata();
+  }, []);
 
-                  
-                </div>
+  // Calculate the range of items to display on the current page
+  const startIndex = (currentPage - 1) * itemsPerPage;
+  const endIndex = startIndex + itemsPerPage;
+  const itemsToDisplay = Profiledata.slice(startIndex, endIndex);
 
-            </div>
+  return (
+    <div className="page-container">
+      <div className="content-container">
+        <div className="pt-8 m-5 rounded-[10px] mt-20 overflow-auto rounded-lg shadow hidden md:block ">
+          {loading && <p>Loading...</p>}
+          {error && <p>Error: {error}</p>}
+          <div className="pt-5 relative overflow-x-auto">
+            {itemsToDisplay.length > 0 ? (
+              <table className="justify-center w-full m-2 bg-darkBlue-500">
+                <thead className="bg-gray-300 text-darkBlue-500">
+                  <tr>
+                    <th className="whitespace-no-wrap text-sm font-semibold tracking-wide text-left  p-4 ">
+                      SNo.
+                    </th>
+                    <th className="whitespace-no-wrap text-sm font-semibold tracking-wide text-left  p-4">
+                      Name
+                    </th>
+                    <th className="whitespace-no-wrap text-sm font-semibold tracking-wide text-left  p-4">
+                      EMail
+                    </th>
+                    <th className="whitespace-no-wrap text-sm font-semibold tracking-wide text-left  p-4">
+                      Contact
+                    </th>
+                    <th className="whitespace-no-wrap text-sm font-semibold tracking-wide text-left  p-4">
+                      Address
+                    </th>
+                  </tr>
+                </thead>
 
-
+                <tbody className="divide-y divide-red-100 ">
+                  {itemsToDisplay.map((data, index) => (
+                    <tr key={index} className="text-White-100">
+                      <td className="whitespace-nowrap text-left p-4 text-sm ">
+                        {data.Sno}
+                      </td>
+                      <td className="whitespace-nowrap text-left p-4 text-sm ">
+                        {data.name}
+                      </td>
+                      <td className="whitespace-nowrap text-left p-4 text-sm ">
+                        {data.email}
+                      </td>
+                      <td className="whitespace-nowrap text-left p-4 text-sm ">
+                        {data.contact}
+                      </td>
+                      <td className="whitespace-nowrap text-left p-4 text-sm ">
+                        {" "}
+                        <div className="flex justify-center items-center h-screen">
+                          <Dropdown />
+                        </div>
+                      </td>
+                    </tr>
+                  ))}
+                </tbody>
+              </table>
+            ) : (
+              <p>No data found</p>
+            )}
+            ;
+          </div>
         </div>
-    );
+      </div>
+    </div>
+  );
 };
 
 export default UserManagement;
